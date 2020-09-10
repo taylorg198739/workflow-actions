1a23d2dab6c8ae2df0107554dfec83416757ae0c
Taylor <taylorg198739@gmail.com>
2020-09-10 03:55:59 -0400

    Test action values

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
+  const issueNumber = core.getInput('issue_number');
+  const repoId = core.getInput('repo_id');
+  const zhToken = core.getInput('zh_token');
+  const zhWorkspaceId = core.getInput('zh_workspace_id');
+  const zhInprogressId = core.getInput('zh_in_progress_id');
+
   console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  const test = `${nameToGreet}, ${issueNumber}, ${repoId}, ${zhToken}, ${zhWorkspaceId}, ${zhInprogressId}`
+
+  core.setOutput("output", test);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
