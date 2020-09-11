8fc6c68287b06dc6c8ffa2fdbb566542af540b1d
Taylor <taylorg198739@gmail.com>
2020-09-11 05:44:19 -0400

    check git ref

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const github = require('@actions/github');
 try {
   const nameToGreet = core.getInput('who-to-greet');
   const issueNumber = core.getInput('issue_number');
+  const gitref = core.getInput('git_ref');
   const repoId = core.getInput('repo_id');
   const zhToken = core.getInput('zh_token');
   const zhWorkspaceId = core.getInput('zh_workspace_id');
   const zhInprogressId = core.getInput('zh_in_progress_id');
 
-  console.log(`all data:  ${nameToGreet}!, ${issueNumber}, ${repoId}, ${zhToken}, ${zhWorkspaceId}, ${zhInprogressId}`);
+  console.log(`all data:  ${nameToGreet}!, ${gitref}, ${issueNumber}, ${repoId}, ${zhToken}, ${zhWorkspaceId}, ${zhInprogressId}`);
   const test = `${nameToGreet}, ${issueNumber}, ${repoId}, ${zhToken}, ${zhWorkspaceId}, ${zhInprogressId}`
 
   core.setOutput("output", test);
