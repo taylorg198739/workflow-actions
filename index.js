be4ae33937608b9b8cfc729c352bfecc72b2d35e
Taylor <taylorg198739@gmail.com>
2020-09-15 04:31:33 -0400

    Update console

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const github = require('@actions/github');
 
 try {
   const nameToGreet = core.getInput('who-to-greet');
-  const issueNumber = core.getInput('issue_number');
   const gitref = core.getInput('git_ref');
   const repoId = core.getInput('repo_id');
   const zhToken = core.getInput('zh_token');
@@ -11,11 +10,11 @@ try {
   const zhInprogressId = core.getInput('zh_in_progress_id');
 
   console.log(`all info data:  ${nameToGreet}!, ${gitref}, ${issueNumber}, ${repoId}, ${zhToken}, ${zhWorkspaceId}, ${zhInprogressId}`);
-  const test = `${nameToGreet}, ${issueNumber}, ${repoId}, ${zhToken}, ${zhWorkspaceId}, ${zhInprogressId}`
+  const test = `${nameToGreet}, ${gitref}, ${repoId = '289868612'}, ${zhToken.length}, ${zhWorkspaceId='***'}, ${zhInprogressId}`
 
   core.setOutput("output", test);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const payload = JSON.stringify(github.context.payload.ref, undefined, 2)
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
