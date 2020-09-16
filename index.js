3ecea757410178d1d1961fd8b586366de2973d7b
Taylor <taylorg198739@gmail.com>
2020-09-16 05:05:22 -0400

    Integrate post

--- index.js
+++ index.js
@@ -20,44 +20,43 @@ try {
     var url = `https://api.zenhub.com/p1/repositories/${repoId}/issues/${issueNumber}`;
     http.open('GET', url);
     console.log('$$$$$$$$$$$$ url: ', url);
-    
-    //Send the proper header information along with the request
-    // http.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     http.setRequestHeader('X-Authentication-Token', `${zhToken}`);
     
     http.onreadystatechange = function() {//Call a function when the state changes.
       console.log('^^^^^^ function started', http.status, http.readyState);
-      if(http.status == 200) {
+      if(httpPost.readyState == 4 && http.status == 200) {
         
-        // alert(http.responseText);
-        console.log('&&&&&&&&&&&& Response Text: ', http.responseText);
+        console.log('&&&&&&&&&&&& Response Text: ', JSON.stringify(http.responseText) );
+        var response = http.responseText;
+
+        // move card
+        if (response.pipeline.name !== "In Progress") {
+          var httpPost = new XMLHttpRequest();
+          var urlPost = `https://api.zenhub.com/p2/workspaces/${zhWorkspaceId}/repositories/${repoId}/issues/${issueNumber}/moves`;
+          var content = {
+            'pipeline_id': zhInprogressId,
+            'position': 'top'
+          }
+          var params = JSON.stringify(content);
+          httpPost.open('POST', urlPost);
+          
+          //Send the proper header information along with the request
+          httpPost.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+          httpPost.setRequestHeader('X-Authentication-Token', `${zhToken}`);
+          
+          httpPost.onreadystatechange = function() {//Call a function when the state changes.
+            console.log('^^^^^^ function POST started', httpPost.status);
+              if(httpPost.readyState == 4 && httpPost.status == 200) {
+                
+                // alert(http.responseText);
+                console.log('&&&&&&&&&&&& Response Text: ', httpPost.responseText);
+              }
+          }
+          httpPost.send(params);
+        }
       }
     }
     http.send(null);
-
-
-    // var http = new XMLHttpRequest();
-    // var url = `https://api.zenhub.com/p2/workspaces/${zhWorkspaceId}/repositories/${repoId}/issues/${issueNumber}/moves`;
-    // var content = {
-    //   'pipeline_id': zhInprogressId,
-    //   'position': 'top'
-    // }
-    // var params = JSON.stringify(content);
-    // http.open('POST', url);
-    
-    // //Send the proper header information along with the request
-    // http.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    // http.setRequestHeader('X-Authentication-Token', `${zhToken}`);
-    
-    // http.onreadystatechange = function() {//Call a function when the state changes.
-    //   console.log('^^^^^^ function started', http.status);
-    //     if(http.readyState == 4 && http.status == 200) {
-          
-    //       // alert(http.responseText);
-    //       console.log('&&&&&&&&&&&& Response Text: ', http.responseText);
-    //     }
-    // }
-    // http.send(params);
   }
 
   core.setOutput("output", isValidBranch);
