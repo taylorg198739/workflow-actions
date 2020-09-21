b13510bed86f480b592c8a0ae1d660b88d3f5579
Taylor <taylorg198739@gmail.com>
2020-09-21 04:03:27 -0400

    Add new action

--- index.js
+++ index.js
@@ -7,51 +7,57 @@ try {
   const zhToken = core.getInput('zh_token');
   const zhWorkspaceId = core.getInput('zh_workspace_id');
   const zhInprogressId = core.getInput('zh_in_progress_id');
-  const issueNumber = parseInt(github.context.payload.ref.split('/')[0], 10);
-  const isValidBranch = typeof issueNumber === 'number';
-  let status = '';
-  console.log('------------ git data: ', JSON.stringify(github));
+  const action_name = core.getInput('action_name');
 
-  if (isValidBranch) {
+  if (action_name === 'create_branch') {
+    const issueNumber = parseInt(github.context.payload.ref.split('/')[0], 10);
+    const isValidBranch = typeof issueNumber === 'number';
+    let status = '';
+    console.log('------------ git data: ', JSON.stringify(github));
 
-    var http = new XMLHttpRequest();
-    var url = `https://api.zenhub.com/p1/repositories/${repoId}/issues/${issueNumber}`;
-    http.open('GET', url);
-    http.setRequestHeader('X-Authentication-Token', `${zhToken}`);
+    if (isValidBranch) {
+      var http = new XMLHttpRequest();
+      var url = `https://api.zenhub.com/p1/repositories/${repoId}/issues/${issueNumber}`;
+      http.open('GET', url);
+      http.setRequestHeader('X-Authentication-Token', `${zhToken}`);
 
-    http.onreadystatechange = function() { //Call a function when the state changes.
-      if(http.readyState == 4 && http.status == 200) {
-        var response = JSON.parse(http.responseText);
-        status = "Fetch Data Success"
+      http.onreadystatechange = function() { //Call a function when the state changes.
+        if(http.readyState == 4 && http.status == 200) {
+          var response = JSON.parse(http.responseText);
+          status = "Fetch Data Success"
 
-        // move card
-        if (response.pipeline.name !== "In Progress") {
-          var httpPost = new XMLHttpRequest();
-          var urlPost = `https://api.zenhub.com/p2/workspaces/${zhWorkspaceId}/repositories/${repoId}/issues/${issueNumber}/moves`;
-          var content = {
-            'pipeline_id': zhInprogressId,
-            'position': 'top'
+          // move card
+          if (response.pipeline.name !== "In Progress") {
+            var httpPost = new XMLHttpRequest();
+            var urlPost = `https://api.zenhub.com/p2/workspaces/${zhWorkspaceId}/repositories/${repoId}/issues/${issueNumber}/moves`;
+            var content = {
+              'pipeline_id': zhInprogressId,
+              'position': 'top'
+            }
+            var params = JSON.stringify(content);
+            httpPost.open('POST', urlPost);
+
+            // Send the proper header information along with the request
+            httpPost.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+            httpPost.setRequestHeader('X-Authentication-Token', `${zhToken}`);
+
+            httpPost.onreadystatechange = function() { //Call a function when the state changes.
+                if(httpPost.readyState == 4 && httpPost.status == 200) {
+                  status = "Move Card Success"
+                }
+            }
+            httpPost.send(params);
           }
-          var params = JSON.stringify(content);
-          httpPost.open('POST', urlPost);
-
-          // Send the proper header information along with the request
-          httpPost.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-          httpPost.setRequestHeader('X-Authentication-Token', `${zhToken}`);
-
-          httpPost.onreadystatechange = function() { //Call a function when the state changes.
-              if(httpPost.readyState == 4 && httpPost.status == 200) {
-                status = "Move Card Success"
-              }
-          }
-          httpPost.send(params);
         }
       }
+      http.send(null);
     }
-    http.send(null);
+
+    core.setOutput("output", status);
+  } else if (action_name === 'merge_to_master') {
+    console.log('------------ git data master: ', JSON.stringify(github));
   }
 
-  core.setOutput("output", status);
 } catch (error) {
   console.log('-- Error --', error.message);
   core.setFailed(error.message);
